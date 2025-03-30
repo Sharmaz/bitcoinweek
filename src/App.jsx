20d1dc9d2f2b7c631b714ecb0ca38f7354dab785
Ivan Robles <irae45@gmail.com>
2025-03-29 23:13:36 -0600

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-5xl text-yellow-400 text-center">Bitcoin Week</h1>
+    </div>
+  );
+}
+
+export default App;
