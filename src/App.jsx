5d5fa1c58e940feaf42863b6f4fbd7bb26c4a7e2
Ivan Robles <irae45@gmail.com>
2025-04-02 17:36:29 -0600

    add location section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./LandingPage/Hero";
 import About from "./LandingPage/About";
+import Location from "./LandingPage/Location";
 function App() {
 
   return (
     <div className="flex flex-col justify-center bg-violet-950">
       <Hero />
       <About />
+      <Location />
     </div>
   );
 }
