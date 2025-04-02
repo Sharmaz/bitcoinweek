d5328fc2e5c615d8cc200e96314f6f4907a7452e
Ivan Robles <irae45@gmail.com>
2025-04-02 14:03:43 -0600

    add hero section responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Hero from "./LandingPage/Hero";
 function App() {
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-violet-950">
-      <h1 className="text-5xl text-yellow-400 text-center">Bitcoin Week</h1>
+    <div className="flex flex-col justify-center bg-violet-950">
+      <Hero />
     </div>
   );
 }
