a1cf4cde7d18268697539ff874d1979b18ecdcaf
Ivan Robles <irae45@gmail.com>
2025-04-02 14:16:27 -0600

    add about section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./LandingPage/Hero";
+import About from "./LandingPage/About";
 function App() {
 
   return (
     <div className="flex flex-col justify-center bg-violet-950">
       <Hero />
+      <About />
     </div>
   );
 }
