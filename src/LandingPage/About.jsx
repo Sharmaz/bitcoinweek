4927cc74590c6f457feda5194abda2a48ea9eed5
Ivan Robles <irae45@gmail.com>
2025-04-04 14:51:30 -0600

    add title decoration, remove separators.

--- src/LandingPage/About.jsx
+++ src/LandingPage/About.jsx
@@ -11,10 +11,8 @@ function About() {
   return (
     <div className={`relative z-10 flex flex-col justify-center items-center transition-all ease-in-out duration-300 ${isVisible ? 'md:opacity-100 md:translate-y-4' : 'md:opacity-0'} md:my-32`} ref={ref}>
       <div className="relative z-10 flex flex-col justify-center items-center text-white w-[300px] md:w-[600px] text-center">
-        <h2 className="text-2xl md:text-3xl font-bold">De que se trata BitcoinWeek?</h2>
+        <h2 className="flex items-center justify-center title relative text-2xl md:text-3xl font-bold px-4 before:w-[300px] before:md:w-[600px] w-[200px] md:w-[auto]">De que se trata BitcoinWeek?</h2>
         <p className="text-lg md:text-2xl my-8 text-left">Durante la semana del Talent Land y un dia extra en el Hacker Garage, tendremos charlas, talleres y eventos (sorpresa) para que todos los participantes puedan aprender sobre el mundo de Bitcoin.</p>
-      </div>
-      <div className={`separator absolute bottom-[-50px] md:bottom-[-140px] w-screen h-16 md:h-[140px] bg-cover transition-all ease-in-out duration-300 ${isVisible ? 'md:opacity-100' : 'md:opacity-0'}`}>
       </div>
     </div>
   );
